4ce8e0c04260142c18da50b80b766317c1828ac1
SunnyDev7 <sunnydev4589@gmail.com>
2024-06-26 20:17:03 +0530

    understanding variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 167283;
+//accountId = 18292 //not allowed
+
+let accountEmail = "sunnydev@yahoo.com";
+var accountPassword = "11111";
+accountCity = "Ahmedabad";
+
+console.table([accountEmail, accountPassword, accountId, accountCity]);
