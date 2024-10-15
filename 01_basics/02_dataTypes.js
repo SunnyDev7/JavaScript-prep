de050b6c96f471a4603e6b460faf5b53734433af
SunnyDev7 <sunnydev4589@gmail.com>
2024-10-15 12:21:23 +0200

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treats all the JS code as newer version of JS
+
+// alert(1+1) //we are using nodejs, not browser so it will not work
+
+let name = "Sunny"; //String
+let age = 26;  //number
+let isLoggedIn = false; //boolean
+let a1;  //undefined
+let a2 = null; // this is possible, explicitly defining the variable as null instaed of undefined
+ //null = Standalone value
+ //undefined = not having any value
+ //symbol = unique 
+ //objects
+
+ console.log(typeof age); // this will give the O/P as number
+ console.log(typeof null); //this will give output as Object
+ 
+ 
