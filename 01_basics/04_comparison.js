d9c87d8b124c8e6f1cba3dfc13c718ec82b5a475
SunnyDev7 <sunnydev4589@gmail.com>
2024-10-29 23:23:27 +0100

    04_comparision

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,28 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// always make usre to keep the datatype of two vars before comparing
+console.log("2">1);
+console.log("2">1);
+
+
+console.log(null>0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//avoid the comparison stated above, as it can cause confusion
+
+//=== this checks striclty and 
+console.log("2" === 2);
+
+
+
+
+
