aec578535edd98748fe13a2f02e1d9e92949b07a
SunnyDev7 <sunnydev4589@gmail.com>
2024-10-29 23:04:17 +0100

    All Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -29,3 +29,46 @@ console.log(typeof stringNumber);
 
 
 
+///////////////////operations****************************************
+
+
+let value = 3;
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2+2);
+// console.log(2**2); //power
+// console.log(2/2);
+// console.log(2%2);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
