a3beef85285af5d6c656c235d8b90e5b3fd685a0
SunnyDev7 <sunnydev4589@gmail.com>
2024-12-27 16:34:54 +0100

    Scope two

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,19 +9,57 @@
 
 //var c = 300
 //golbal scope
-let a = 300
+let a = 300;
 
-if (true) { //block scope
+if (true) {
+  //block scope
   let a = 10;
   const b = 20;
   var c = 30;
   console.log("Inner", a);
-  
 }
 
 //console.log(a); //this will give o/p as a is undefined
 //console.log(b); //this will give o/p as b is undefined
 console.log("Outer", a); //but this will print the value of c which is a problem
 
+//note: Global scope in web browser is Window, and in node env is different.
 
-//note: Global scope in web browser is Window, and in node env is different. 
+//+++++++++++++++++++++++++++++++++++ Scope two ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function one() {
+  const username = "Sunny";
+
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+
+  //console.log(website);
+
+  two();
+}
+
+one();
+
+
+// below is interesting topic
+// there are two ways you can define function, one is simple and other is inside a const or let or var
+
+//1
+
+console.log(addOne(5)) // this will just return the valeu and nothing else, even if called before func declaration
+function addOne(num){
+    return num + 1;
+}
+
+
+//2
+addTwo(5) // but this calling before declaration will give error 
+const addTwo = function(num){
+    return num + 2;
+}
+
+// the above phenomena is called Hoisting
+
+
